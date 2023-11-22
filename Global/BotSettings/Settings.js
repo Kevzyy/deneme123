febd20318f175aa299f4f4d6e72aebf1e335ae82
Kevzyy <139994595+Kevzyy@users.noreply.github.com>
2023-11-22 20:08:27 +0300

    Add files via upload

---
+++ Global/BotSettings/Settings.js
@@ -0,0 +1,104 @@
+module.exports = {
+ 
+    GuildID: "1166450949498339438",
+    GuildName: "PRİV",
+    owners: ["952277585369383112"],
+    BotDurum: ["Kevzyy ❤️"], 
+    BotSesKanal: "1168552479735160902",
+    AltBaşlık: "Kevzyy ❤️",
+    
+DatabaseURL: "mongodb+srv://herdem9226:herdem8226@cluster0.g64lrnt.mongodb.net/xdsss?retryWrites=true&w=majority",
+    
+        Welcome: {
+            Active: true,
+            Tokens: [
+               "",
+               ""
+    
+            ],
+            Channels: [
+                "1162577440539951142",
+                "1162577440539951143"
+            ]
+        },
+    
+    
+       
+    Main: {
+        ModerationToken: "OTY1Mjk2NzIxMDQxMTYyMjcw.G6D4Hw.k1fO1rmIPGBKTklDZXiPmyet0m2EOFuqsVOCJk",
+        RegisterToken: "OTY1Mjk5NjI2NTExMDczMzIw.GrY16m.cdV8bXDrFrcz6it7Vb5sa-1uiHF1mp0zV4nmLk",
+        StatsToken: "OTY1Mjk2OTY4MDM5NTQ2OTEw.GQFd_l.Az7gZN0iu9VMIK1wCDOGd6KI-7_F2pkDBPtPk0",
+
+        prefix: [
+            ".",
+            "!"
+        ],
+
+        BotClientID: "965296721041162270",
+    
+            dmMessages: true,
+            LevelSystem: true,
+    
+            messageDolar: 1,
+            voiceDolar: 1,
+          
+            messageCount: 1,
+            messageCoin: 2,
+            voiceCount: 1,
+            voiceCoin: 4,
+            publicCoin: 4,
+            inviteCount: 1,
+            inviteCoin: 15,
+            taggedCoin: 25,
+            toplamsCoin: 5.5,
+            yetkiCoin: 30,
+          
+            banlimit: 3,
+            jaillimit: 3,
+            warnlimit: 3,
+            chatmutelimit: 3,
+            voicemutelimit: 3,
+        
+        },
+    
+        Guard: {
+            Token: {
+                Guard_I: "MTE3MTA4NTkyNTIyOTUzNTMxMg.G4WmpM.CMMm9pVrDs0Q4X47w9BTkxoZBskQ0PBh3D5BKU",
+                Guard_II: "MTE3MTA4NTkyNTIyOTUzNTMxMg.G4WmpM.CMMm9pVrDs0Q4X47w9BTkxoZBskQ0PBh3D5BKU",
+                Guard_III: "MTE3MTA4NTkyNTIyOTUzNTMxMg.G4WmpM.CMMm9pVrDs0Q4X47w9BTkxoZBskQ0PBh3D5BKU",
+    
+                TaçHesapToken: "", 
+                UrlGuardToken: "",
+    
+                Dağıtıcı: [
+                   ""
+                ]
+            },
+    
+            Limit: {
+                Ban: 1,
+                Kick: 1,
+                RoleCreate: 1,
+                ChannelCreate: 2,
+    
+                Reklam: 5,
+                Etiket: 5,
+                Satır: 5,
+                Ekler: 5
+            },
+    
+            Guard: {
+                TaskDelay: 250,
+                GiveRoleDelay: 500
+            },
+    
+            kufurEngel: true,
+            spamEngel: true,
+            capsEngel: true,
+            reklamEngel: true
+    
+        }
+    }
+
+
+
---
+++ Global/Settings/Bot-Commands.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+CommandChannel: ["commands"],
+
+
+}
